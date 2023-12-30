6d78774e088b3016baee4c8b220875314f20dca8
bimoware <ptdrrrrr.t.ki@gmail.com>
2023-12-30 02:48:40 +0100

    First com

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 class ChessGame {
     constructor(fenPos){
-        /*
-        r = rook
-        n = knight
-        b = bishop
-        q = queen
-        k = king
-        
-        uppercase = black
-        lowercase = white*/
         this.startPos = fenPos || 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
     }
 }
