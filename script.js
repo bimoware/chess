a0b33480a8e63d438dd058ddd06df5176fa5d6ee
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-01-02 17:38:36 +0100

    Fixed matrix

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 class ChessGame {
-    constructor(fenPos){
-        this.startPos = fenPos || 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
-
-    }
-    get matrix(){
-        return fenPos.split('/').map(lign => lign.split(''))
-    }
+    /**
+     * @param {string} fen
+     */
+  constructor(fen) {
+    this.startPos = fen || "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+  }
+  /**
+   * @returns {string[][]}
+   */
+  get matrix() {
+    return this.startPos.split("/").map((lign) => {
+      let newLine = [];
+      for (let elem of lign.split("")) {
+        if(Number(elem)) newLine.push(...(new Array(Number(elem))))
+        else newLine.push(elem)
+      }
+      return newLine
+    });
+  }
 }
 
-const game = new ChessGame()
+const game = new ChessGame();
