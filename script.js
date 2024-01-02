7ca8d0a1e3f80f1cdfc839c4d724aa9ae3996ad8
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-01-02 17:18:52 +0100

    Added Matrix

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 class ChessGame {
     constructor(fenPos){
         this.startPos = fenPos || 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+        
     }
     get matrix(){
         return fenPos.split('/').map(lign => lign.split(''))
